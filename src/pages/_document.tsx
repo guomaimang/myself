4d0f8530f0633e949009b643660e46d20a0c1085
hanjiaming <47519540+guomaimang@users.noreply.github.com>
2025-03-19 02:44:48 +0800

    update

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/myself/favicon.ico" />
+        <link rel="apple-touch-icon" href="/myself/icons/apple-touch-icon.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
