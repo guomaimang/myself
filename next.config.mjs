654fb2c8debb3e45ccea518a4b3154aa1737687a
hanjiaming <47519540+guomaimang@users.noreply.github.com>
2025-03-19 21:55:21 +0800

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,36 +1,6 @@
 /** @type {import('next').NextConfig} */
-import PWABuilder from 'next-pwa';
 
-const withPWA = PWABuilder({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  runtimeCaching: [
-    {
-      urlPattern: /\.(css|js)$/i,
-      handler: 'CacheFirst',
-      options: {
-        cacheName: 'static-assets',
-      },
-    },
-    {
-      urlPattern: /\.(png|jpg|jpeg|svg|gif|webp)$/i,
-      handler: 'CacheFirst',
-      options: {
-        cacheName: 'images',
-      },
-    },
-    {
-      urlPattern: /\.(woff|woff2|eot|ttf|otf)$/i,
-      handler: 'CacheFirst',
-      options: {
-        cacheName: 'fonts',
-      },
-    },
-  ],
-});
-
-const nextConfig = withPWA({
+const nextConfig = {
   reactStrictMode: true,
   output: "export",
   images: {
@@ -68,6 +38,6 @@ const nextConfig = withPWA({
       },
     ];
   },
-});
+};
 
 export default nextConfig;
