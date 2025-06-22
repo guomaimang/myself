c2afaead43dc63b1d6a9cb300d04963e3802fb08
hanjiaming <47519540+guomaimang@users.noreply.github.com>
2025-06-22 21:26:30 +0800

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,22 +6,22 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title> About Myself | Hirsun HAN</title>
+        <title> About Myself | Hirsun HAN Jiaming</title>
         <link rel="manifest" href="/myself/manifest.json" />
-        <meta name="application-name" content="Hirsun's Portfolio" />
+        <meta name="application-name" content="Hirsun's Portfolio (HAN Jiaming)" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta
           name="apple-mobile-web-app-title"
-          content="Hirsun's Portfolio"
+          content="Hirsun's Portfolio (HAN Jiaming)"
         />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="theme-color" content="#000000" />
         <meta name="baidu-site-verification" content="codeva-a05qwHvmi1" />
-        <meta name="keywords" content="Hirsun HAN, 韩佳明, HANJIAMING" />
+        <meta name="keywords" content="Hirsun, HAN Jiaming, 韩佳明" />
         <meta
           name="description"
-          content="Hi, I'm Hirsun HAN, from Hong Kong."
+          content="Hi, I'm Hirsun (HAN Jiaming), from Hong Kong."
         />
       </Head>
       <Component {...pageProps} />
